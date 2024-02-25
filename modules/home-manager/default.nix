4938def8196144872178fbd50e1ef2685f1dcc4f
Will Czifro <will@czifro.me>
2024-02-24 17:58:10 -0800

    Propagate downstream home packages list

--- modules/home-manager/default.nix
+++ modules/home-manager/default.nix
@@ -8,7 +8,7 @@
   home = {
     packages = [
       pkgs.neovim
-    ];
+    ] ++ configs.home.packages;
 
     # This is an option, but I prefer splitting it out into neighboring nix files
     # file = {
