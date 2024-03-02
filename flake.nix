42fbfc059c5af703d350a950c0d3b689f0c467af
Will Czifro <will@czifro.me>
2024-03-01 19:34:27 -0800

    Make nvim home-manager config standalone

--- flake.nix
+++ flake.nix
@@ -3,13 +3,33 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+
+    home-manager = {
+      url = "github:nix-community/home-manager/master";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
-  outputs = { nixpkgs, ... } @ inputs: {
+  outputs = { nixpkgs, home-manager, ... } @ inputs:
 
+  let
+
+  pkgs = import nixpkgs { system = "aarch64-darwin"; };
+
+  in
+
+  {
     modules = {
-      nvim = import ./modules/home-manager;
+      home-manager = {
+        nvim = import ./modules/home-manager;
+      };
     };
 
+    homeConfigurations = {
+      czifro = home-manager.lib.homeManagerConfiguration {
+        inherit pkgs;
+        modules = [ modules.home-manager.nvim ];
+      };
+    };
   };
 }
